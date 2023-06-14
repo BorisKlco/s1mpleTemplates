1ab274c502330bdf575ed649ad8ca12e891e480d
Boris Klco <klco@pm.me>
2023-06-14 12:39:00 +0000

    setting up search

--- adoptme/src/App.jsx
+++ adoptme/src/App.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
+import Search from "./Search";
 
 const App = () => {
     return (
         <div>
             <h1>Hi</h1>
+            <Search />
         </div>
     )
 }
