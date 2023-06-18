4d528ac0e2f8f34f809073b7d5c0b680f6b294a6
Boris Klco <klco@pm.me>
2023-06-18 18:16:43 +0000

    busta is donzo

--- wsop/src/Video.jsx
+++ wsop/src/Video.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import fetchVideo from "./fetchVideo";
@@ -6,16 +7,16 @@ const Video = () => {
   const { id } = useParams();
   const { data, isLoading } = useQuery(["video", id], fetchVideo);
 
-  console.log(data);
-
   if (isLoading) return "Loading...";
 
   const video = data.playlist[0].sources[3];
-  console.log(video);
+  console.log(data.playlist[0]);
 
   return (
     <div>
-      <video src={video.file}></video>
+      <video>
+        <source src={video.file} type="video/mp4" />
+      </video>
     </div>
   );
 };
