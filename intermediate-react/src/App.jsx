d3d787edcaee72a7713758f7c9e9aef786868a00
Boris Klco <klco@pm.me>
2023-08-18 20:04:23 +0200

    useMemo

--- intermediate-react/src/App.jsx
+++ intermediate-react/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useMemo, useEffect } from "react";
 import "./App.css";
 
+const fibonacci = (n) => {
+  if (n <= 1) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(23);
+  const [left, setLeft] = useState(0);
   const refDom = useRef(null);
+  const memoexample = useMemo(() => fibonacci(count), [count]);
+
+  useEffect(() => {
+    requestAnimationFrame(animate);
+    function animate() {
+      setLeft(left + 1);
+    }
+  }, [left]);
 
   const handleButtonClick = () => {
     setCount((count) => count + 1);
@@ -26,6 +43,15 @@ function App() {
         <p>Sample Text</p>
         <p>Sample Text</p>
         <button onClick={handleRefDom}>Click for focus</button>
+      </div>
+
+      <div>
+        <p>useMemo example</p>
+        <div
+          style={{ left: `${Math.sin(left * 0.05) * 100 + 100}px` }}
+          className="ball"
+        ></div>
+        <p>current fibonacci num {memoexample}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
